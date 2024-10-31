2dcb36715ac5c8b00bc2b4dde39ae4a4598b3997
Berbash-Natalia <berbasn@gmail.com>
2024-10-31 14:34:33 +0300

    Commit - 4

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -4,6 +4,9 @@ export const User = () => {
     return (
         <div>
             <h2>User2323</h2>
+            <h2>User2323</h2>
+            <h2>User2323</h2>
+            <h2>User2323</h2>
 
         </div>
     )
